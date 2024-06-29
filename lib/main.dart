f9f00148a27024863583b72b970b8058b22b48d5
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-29 20:28:08 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:add_to_cart/view/add_to_cart_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: AppColors.primaryColor,),);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+    return ScreenUtilInit(
+      designSize: Size(360 ,746),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Add To Cart Design',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          textTheme: GoogleFonts.interTextTheme(),
+        ),
+        home: AddToCartScreen(),
+
+      )
+    );
+  }
+}
