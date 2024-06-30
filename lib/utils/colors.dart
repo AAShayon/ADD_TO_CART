79a77713ecfbdc7e6cba4da4e7000ede85f6720c
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-30 10:42:14 +0600

    add--utils

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -7,10 +7,13 @@ class AppColors{
   static const Color primaryColor=Color(0xffFF6B23);
   static const Color  secondaryColor=Color(0xffFF9727);
   static const Color baseColorOffWhite=Color(0xffF7F8F9);
+  static const Color baseColorOffWhiteShade=Color(0xff686868);
+  static const Color baseColorWhite=Color(0xffffffff);
+  static const Color baseColorBlack=Color(0xff010832);
   static const Color baseColorLightOrange=Color(0xffFFF1E9);
   static const LinearGradient linearGradient=LinearGradient(colors: [
     Color(0xffFF6B23),Color(0xffFF9727),],
-    stops: [0.9, 0.9],
+    stops: [0.0, 1.0],
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
   );
