85e937283ff6f634c536701cf5c435a9dd27fb4e
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-30 10:43:21 +0600

    update--all

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:add_to_cart/view/add_to_cart_screen.dart';
+import 'package:add_to_cart/viewModel/food_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +15,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.white,),);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.white),
+    );
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
+      DeviceOrientation.portraitDown,
     ]);
+
     return ScreenUtilInit(
-      designSize: Size(360 ,746),
+      designSize: const Size(360, 746),
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Add To Cart Design',
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
-          textTheme: GoogleFonts.interTextTheme(),
-        ),
-        home: AddToCartScreen(),
-
-      )
+      builder: (context, child) {
+        return ChangeNotifierProvider(
+          create: (context) => FoodViewModel(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Add To Cart Design',
+            theme: ThemeData(
+              primarySwatch: Colors.grey,
+              textTheme: GoogleFonts.interTextTheme(),
+            ),
+            home: const AddToCartScreen(),
+          ),
+        );
+      },
     );
   }
 }
